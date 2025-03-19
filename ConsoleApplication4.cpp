48c17120016aa6cb4dc9ab29b14346819505e371
NikTom23 <Tomilov15kow10@gmail.com>
2025-03-19 21:34:56 +0300

    Add files via upload

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main()
+{
+    float n[6] = { -7,6,9,-11,20,-1 };
+        //for (int i = 0; i < 6; i++)
+        //{ 
+        //        std::cout << "Sl " << i << " :" << n[i] << std::endl;
+        //    }
+      std::cout << n[0] <<", " << n[3] <<", " << n[5] << std::endl;
+      std::cout << n[1] << ", " << n[2] << ", " << n[4] << std::endl;
+}
